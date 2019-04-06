e2a14ecb4d5b614dce878c1be9ad2b0a60d7dc23
fajlerajib <fajlerajib@192.168.36.140>
2019-04-06 01:17:27 -0400

    "HOME WORK" Find out Even number and ODD Number.

---
+++ Study_Smart/src/com/dailypractise/Practise_Even_OddNumber.java
@@ -0,0 +1,27 @@
+package com.dailypractise;
+
+// HOME WORK //
+
+public class Practise_Even_OddNumber {
+
+	public static void main(String[] args) {
+
+		// Even and Odd number
+
+		for (int i = 0; i < 200; i++) {
+
+			// System.out.println(i);
+
+			if (i % 2 == 0) {
+				System.out.println("  ---------------------  ");
+				System.out.println("This is a Even Number :" + i + ",");
+
+			} else {
+				System.out.println("This is a ODD Number  :" + i + ",");
+
+			}
+
+		}
+
+	}
+}
